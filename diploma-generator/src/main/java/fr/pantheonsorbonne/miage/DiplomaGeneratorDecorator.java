00e43e6803c3b4c03e622882c7a351385c2fbe74
dieynaba-sudo <56385640+dieynaba-sudo@users.noreply.github.com>
2019-12-18 20:43:48 +0100

    Update DiplomaGeneratorDecorator.java
    This decorator pattern allow you to encrypt whatever DiplomaGenerator…

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaGeneratorDecorator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaGeneratorDecorator.java
@@ -1,5 +1,15 @@
 package fr.pantheonsorbonne.miage;
 
+/**
+ * This decorator pattern allow you to encrypt whatever DiplomaGenerator by providing a password.
+ * to use it, simply replace
+ * DiplomaGenerator generator = new MiageDiplomaGenerator(...);
+ * by
+ * EncryptedDiplomaGeneratorDecorator generator = new EncryptedDiplomaGeneratorDecorator(new MiageDiplomaGenerator(...)); 
+ * @author nherbaut
+ *
+ */
+
 public abstract class DiplomaGeneratorDecorator extends AbstractDiplomaGenerator {
 
 	protected DiplomaGenerator other;
