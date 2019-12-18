c94af3daccf278e3c9b85f12c6080e353cb06cd9
dieynaba-sudo <56385640+dieynaba-sudo@users.noreply.github.com>
2019-12-18 20:09:56 +0100

    Update StudentRepository.java

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -3,6 +3,9 @@ package fr.pantheonsorbonne.miage;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
@@ -18,6 +21,9 @@ import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 public class StudentRepository implements Iterable<Student> {
+	if (!Files.exists(Paths.get(db))) {
+			throw new RuntimeException("failed to find" + Paths.get(db).toAbsolutePath().toString());
+		}
 
 	private String db;
 	private java.util.Iterator<Student> currentIterator = null;
